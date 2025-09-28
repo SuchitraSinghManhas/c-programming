206f867e5f9493e0529964d3bba8643aa106b797
SuchitraSinghManhas <2025a6r052@mietjammu.in>
2025-09-28 21:48:20 +0530

    Create temperature conversion.c

---
+++ temperature conversion.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    float tempc,tempf;
+    scanf("%f",&tempc);
+    tempf=(tempc*9/5)+32;
+    printf("%.2f",tempf);
+     
+    return 0;
+}
